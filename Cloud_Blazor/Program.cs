b14aa638595d96641cac56a3870237648f1d632f
LinusLund <lundlinus@live.se>
2023-11-28 10:39:34 +0100

    trying tailwind

--- Cloud_Blazor/Program.cs
+++ Cloud_Blazor/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
+
 builder.Services.AddHttpClient("TestingRestAPI", client =>
 {
     client.BaseAddress = new Uri("https://localhost:7029/");
